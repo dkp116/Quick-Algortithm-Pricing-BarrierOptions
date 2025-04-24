b1c86c48c9dc0ce23723f5b20142f17780f86c82
dkp116 <daneel668@gmail.com>
2025-04-24 21:40:45 +0100

    added function to estimate gi

---
+++ build/CMakeFiles/Main.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/dkp116/Desktop/Master York/Diss/code/Intergral Check/Option.cpp" "CMakeFiles/Main.dir/Option.cpp.o" "gcc" "CMakeFiles/Main.dir/Option.cpp.o.d"
+  "/Users/dkp116/Desktop/Master York/Diss/code/Intergral Check/Stock.cpp" "CMakeFiles/Main.dir/Stock.cpp.o" "gcc" "CMakeFiles/Main.dir/Stock.cpp.o.d"
+  "/Users/dkp116/Desktop/Master York/Diss/code/Intergral Check/main.cpp" "CMakeFiles/Main.dir/main.cpp.o" "gcc" "CMakeFiles/Main.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
