92888d3967bcf08b038834710b1db7c92be52106
dkp116 <daneel668@gmail.com>
2025-10-12 19:27:01 +0100

    #2 - added jumpsize and jumptime functions to mjd

--- MetonJumpDynamics.cpp
+++ MetonJumpDynamics.cpp
@@ -1,6 +1,7 @@
 #include "IDynamics.h"
 #include "Random_Generator.h"
 
+
 class MertonJumpDynamics : public IDynamics{
     private:
     double lambda_;  //Frequency of jump 
@@ -24,6 +25,27 @@ class MertonJumpDynamics : public IDynamics{
     return  std::exp(( riskfree_  - 0.5 * sigma_ * sigma_) * TimeIncrement +
                             sigma_ * std::sqrt(TimeIncrement) * generate);   
     }
+
+    double Jumpsize(double JumpMu , double JumpSigma){      
+    std::normal_distribution <> d(JumpMu,JumpSigma);       
+     return  d(RandomGenerator::getGenerator());
+    }  
+
+    std::vector<double> createJumpTimes() {      
+    std::vector<double> Times;
+    std::exponential_distribution<> exp_dis(lambda_);
+    double count = 0.0;
+    Times.push_back(count);
+    while (count <= 1) {
+        double time = exp_dis(RandomGenerator::getGenerator());
+        if (count + time > 1) break;  // Stop if exceeding 1
+        count += time;
+        Times.push_back(count);  // Store cumulative jump time
+    }
+    Times.push_back(1.0);
+    return Times;
+}
+
 
     
 };
