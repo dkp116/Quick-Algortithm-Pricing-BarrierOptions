e37bb02170b28c11a2948881648efed4882f20b0
Daneel <daneel668@gmail.com>
2026-02-18 21:54:08 +0000

    #13 - added unit tests for standard monte carlo

--- new/tests/test_utils/TestDownAndOutPricing.cpp
+++ new/tests/test_utils/TestDownAndOutPricing.cpp
@@ -14,7 +14,7 @@ double price_down_and_out_call_with_taylor_series() {
     );
 
     // Create Monte Carlo pricing engine
-    TaylorApproximation pricing(s, b, 100000);  // 1 million samples
+    TaylorApproximation pricing(s, b, 100000);  
 
     // Compute the price
     return pricing.Price();
@@ -33,8 +33,27 @@ double price_down_and_out_call_with_uniform_distribution() {
     );
 
     // Create Monte Carlo pricing engine
-    UniformSample pricing(s, b, 100000);  // 1 million samples
+    UniformSample pricing(s, b, 100000);  
 
     // Compute the price
     return pricing.Price();
+}
+
+double price_down_and_out_call_with_standard_monte_carlo(){
+     auto dynamic = std::make_shared<MertonJumpDynamics>(0.05, 0.25, 2, 0, 0.1);
+
+    // Create stock with starting price and dynamics
+    auto s = std::make_shared<Stock>(100.0, dynamic);
+
+    // Define a Down-and-Out European call option
+    std::shared_ptr<Option> b = std::make_shared<DownAndOut>(
+        ExerciseType::European, OptionType::Call, 110, 85, 1.0
+    );
+
+    // Create Monte Carlo pricing engine
+    StandardMonteCarlo pricing(s, b, 100000);  
+
+    // Compute the price
+    return pricing.Price();
+
 }
