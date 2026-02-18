32fcdb4dec5ce99ce29ff991e73d788ce0d733b6
Daneel <daneel668@gmail.com>
2026-02-18 21:32:48 +0000

    #13 - added montecarlo header and algorithm

--- new/include/PricingAlgorithm/StandardMonteCarlo.h
+++ new/include/PricingAlgorithm/StandardMonteCarlo.h
@@ -2,12 +2,23 @@
 #define STANDARDMONTECARLO_H
 #include "PricingAlgorithm/IPricing.h"
 #include "Stock/Stock.h"
+#include "Stock/Stock.h"
+#include "Dynamics/IDynamics.h"
+#include "Dynamics/MertonJumpDynamics.h"
+#include "Options/Barrier.h"
+
 
 class StandardMonteCarlo : public IPricing{
     private:
     double iteration_;
+    std::shared_ptr<IDynamics> stockDynamics_;
+    std::shared_ptr<MertonJumpDynamics> mertonDynamics_;
+    std::shared_ptr<DownAndOut> downAndOut_;
     public:
-    StandardMonteCarlo(std::shared_ptr<Stock> stock , std::shared_ptr<Option> option , double iteration) : IPricing(stock, option) , iteration_(iteration) {}
+    StandardMonteCarlo(std::shared_ptr<Stock> stock , std::shared_ptr<Option> option , double iteration) : IPricing(stock, option) , iteration_(iteration), stockDynamics_(stock->GetDynamic()) {
+        mertonDynamics_ = std::dynamic_pointer_cast<MertonJumpDynamics>(stockDynamics_);
+        downAndOut_ = std::dynamic_pointer_cast<DownAndOut>(option_);
+    }
     double Price() override;        
 };
 
