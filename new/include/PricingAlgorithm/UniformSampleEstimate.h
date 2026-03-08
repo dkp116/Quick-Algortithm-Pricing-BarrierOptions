301dcb9de6ad3935b6e07f8e6779b4a6de80a0a9
Daneel <daneel668@gmail.com>
2026-03-08 15:58:51 +0000

    #renamed Varience to Standard error to reflect what is actually being calculared and outputted this to private memeber variable in pricing

--- new/include/PricingAlgorithm/UniformSampleEstimate.h
+++ new/include/PricingAlgorithm/UniformSampleEstimate.h
@@ -19,7 +19,7 @@ private:
 public:
     UniformSample(std::shared_ptr<Stock> stock,
                   std::shared_ptr<Option> option,
-                  double iteration, VarianceCalculation isVarienceIncluded,  Time isTimeIncluded)
+                  double iteration, StandardErrorCalculation isVarienceIncluded,  Time isTimeIncluded)
         : IPricing(stock, option, iteration , isVarienceIncluded, isTimeIncluded),
           stockDynamics_(stock_->GetDynamic())
     {
