0e301894202be5639ce2f8b006be26e9d861eb42
Daneel <daneel668@gmail.com>
2026-03-23 19:20:08 +0000

    #10 - added intergration test for variance reduction

--- new/tests/test_utils/TestDownAndOutPricing.h
+++ new/tests/test_utils/TestDownAndOutPricing.h
@@ -14,3 +14,4 @@ double price_down_and_out_call_with_taylor_series();
 double price_down_and_out_call_with_uniform_distribution();
 double price_down_and_out_call_with_standard_monte_carlo();
 double price_down_and_out_call_with_standard_monte_carlo_and_varience();
+double price_down_and_out_call_with_variance_reduction_using_Uniform_dist();
